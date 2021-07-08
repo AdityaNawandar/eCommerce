510b571f72f7e7f8ab9435d3e006b07e8d684150
whoadityanawandar <nawandaraditya60@gmail.com>
2021-07-08 23:06:40 +0530

    Registration implementation done - lost a lot of time trying to figure out why data wasn't getting written to the db.

--- app/src/main/java/com/whoadityanawandar/ecommerce/RegisterActivity.kt
+++ app/src/main/java/com/whoadityanawandar/ecommerce/RegisterActivity.kt
@@ -1,21 +1,26 @@
 package com.whoadityanawandar.ecommerce
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
+import androidx.core.widget.ContentLoadingProgressBar
+import com.google.firebase.database.*
+import java.lang.NullPointerException
 
-class RegisterActivity : AppCompatActivity()
-{
-    lateinit var edttxtFirstName : EditText
-    lateinit var edttxtLastName : EditText
-    lateinit var edttxtPhoneNumber : EditText
-    lateinit var edttxtPassword : EditText
+class RegisterActivity : AppCompatActivity() {
+    lateinit var edttxtFirstName: EditText
+    lateinit var edttxtLastName: EditText
+    lateinit var edttxtPhoneNumber: EditText
+    lateinit var edttxtPassword: EditText
 
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+    private lateinit var loadingProgressBar: ContentLoadingProgressBar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
@@ -23,38 +28,97 @@ class RegisterActivity : AppCompatActivity()
         edttxtLastName = findViewById(R.id.edttxtLastName)
         edttxtPhoneNumber = findViewById(R.id.edttxtPhone)
         edttxtPassword = findViewById(R.id.edttxtPassword)
+
+        loadingProgressBar = ContentLoadingProgressBar(this)
     }
 
-    fun register(view: View){
+    fun register(view: View) {
         var strFirstName = edttxtFirstName.text.toString()
         var strLastName = edttxtLastName.text.toString()
         var strPhoneNumber = edttxtPhoneNumber.text.toString()
         var strPassword = edttxtPassword.text.toString()
-
-        if(strFirstName.isEmpty()){
+        loadingProgressBar.show()
+        if (strFirstName.isEmpty()) {
             Toast.makeText(this, "Please tell us your name", Toast.LENGTH_SHORT).show()
             return
-        }
-        else if(strPhoneNumber.isEmpty())
-        {
+        } else if (strPhoneNumber.isEmpty()) {
             Toast.makeText(this, "We will need your phone number", Toast.LENGTH_SHORT).show()
             return
-        }
-        else if(strPassword.isEmpty())
-        {
+        } else if (strPassword.isEmpty()) {
             Toast.makeText(this, "Please provide a password", Toast.LENGTH_SHORT).show()
             return
-        }
-        else{
+        } else {
+            //loadingProgressBar.show()
             //register account
-            checkIfUserExists(strPhoneNumber)
+            checkIfUserExists(strPhoneNumber, strFirstName, strLastName, strPassword)
         }
 
     }
 
-    private fun checkIfUserExists(strPhoneNumber: String)
-    {
+    private fun checkIfUserExists(
+        strPhoneNumber: String,
+        strFirstName: String,
+        strLastName: String,
+        strPassword: String
+    ) {
+        try {
+            val usersRef = FirebaseDatabase.getInstance("https://ecommerce-whoadityanawandar-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("Users")
+            val userListener = object : ValueEventListener {
 
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    try {
+                        if (!(snapshot.child(strPhoneNumber).exists())) {
+
+                            var userDetails = hashMapOf<String, Any>(
+                                "firstname" to strFirstName,
+                                "lastname" to strLastName,
+                                "phone" to strPhoneNumber,
+                                "password" to strPassword
+                            )
+
+                            usersRef.child(strPhoneNumber)
+                                .setValue(userDetails)
+                                .addOnCompleteListener { task ->
+                                    if(task.isSuccessful){
+                                        //loadingProgressBar.hide()
+                                        Toast.makeText(this@RegisterActivity, "Congratulations! Your account has been created.", Toast.LENGTH_SHORT).show()
+                                        val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+                                        startActivity(intent)
+                                        //finish()
+                                    }
+                                    else{
+                                        Toast.makeText(this@RegisterActivity,"Something went wrong!", Toast.LENGTH_SHORT).show()
+                                    }
+                                }
+
+
+                        } else {
+                            //loadingProgressBar.hide()
+                            Toast.makeText(
+                                this@RegisterActivity,
+                                "Number already exists. Please try again using another number.",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+                    catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Log.w(TAG, "loadPost:onCancelled", error.toException())
+
+                }
+
+            }
+
+            usersRef.addValueEventListener(userListener)
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
 
