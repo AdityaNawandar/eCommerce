5137a3a9c161497b093a699e10fed184f935322b
whoadityanawandar <nawandaraditya60@gmail.com>
2021-07-10 03:24:01 +0530

    Menu with NoActionBar --> (toolbar) - implemented. Also checking in 5 new files created earlier today/yesterday.

---
+++ app/src/main/java/com/whoadityanawandar/ecommerce/HomeActivity.kt
@@ -0,0 +1,46 @@
+package com.whoadityanawandar.ecommerce
+
+import com.whoadityanawandar.ecommerce.R
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
+
+
+class HomeActivity : AppCompatActivity() {
+
+    lateinit var toolbar: Toolbar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        createMenu()
+
+    }
+
+    private fun createMenu() {
+        toolbar = findViewById(R.id.toolbar)
+        //toolbar.title = ""
+        toolbar.inflateMenu(R.menu.menu)
+        toolbar.setOnMenuItemClickListener { item ->
+            if (item!!.itemId == R.id.logout) {
+                clearSharedPreferences()
+                val intent = Intent(applicationContext, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            false
+        }
+    }
+
+
+    private fun clearSharedPreferences() {
+        val editor = getSharedPreferences("name", MODE_PRIVATE).edit()
+        editor.putString("password", "")
+        editor.putString("phone", "")
+        editor.putBoolean("isLoggedIn", false)
+        editor.apply()
+
+    }
+}
