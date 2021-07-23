f2f8f3fc0386f346ed9f4738e75fb0c5a5d92f8c
whoadityanawandar <nawandaraditya60@gmail.com>
2021-07-23 22:30:04 +0530

    Navigation Drawer implemented.

--- app/src/main/java/com/whoadityanawandar/ecommerce/HomeActivity.kt
+++ app/src/main/java/com/whoadityanawandar/ecommerce/HomeActivity.kt
@@ -5,42 +5,57 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
 
+import com.whoadityanawandar.ecommerce.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
 
-    lateinit var toolbar: Toolbar
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
 
-        createMenu()
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-    }
+        setSupportActionBar(binding.appBarHome.toolbar)
 
-    private fun createMenu() {
-        toolbar = findViewById(R.id.toolbar)
-        toolbar.title = "Home"
-        toolbar.inflateMenu(R.menu.menu)
-        toolbar.setOnMenuItemClickListener { item ->
-            if (item!!.itemId == R.id.logout) {
-                clearSharedPreferences()
-                val intent = Intent(applicationContext, LoginActivity::class.java)
-                startActivity(intent)
-                finish()
-            }
-            false
+        binding.appBarHome.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
         }
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_content_home)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_categories, R.id.nav_orders, R.id.nav_settings, R.id.nav_logout
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.home, menu)
+        return true
+    }
 
-    private fun clearSharedPreferences() {
-        val editor = getSharedPreferences("name", MODE_PRIVATE).edit()
-        editor.putString("password", "")
-        editor.putString("phone", "")
-        editor.putBoolean("isLoggedIn", false)
-        editor.apply()
-
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_home)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/com/whoadityanawandar/ecommerce/ui/home/HomeFragment.kt
@@ -0,0 +1,47 @@
+package com.whoadityanawandar.ecommerce.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import com.whoadityanawandar.eCommerce.ui.home.HomeViewModel
+import com.whoadityanawandar.ecommerce.databinding.FragmentHomeBinding
+import com.whoadityanawandar.ecommerce.R
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val recyclerView = root.findViewById<RecyclerView>(R.id.recvwProducts)
+
+/*        val textView: TextView = binding.textHome
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })*/
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
