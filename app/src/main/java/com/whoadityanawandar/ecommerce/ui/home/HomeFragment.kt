757ae2271f206749d3316a444d71a04d2669c949
whoadityanawandar <nawandaraditya60@gmail.com>
2021-07-24 00:32:39 +0530

    Customer Home Page - Recycler view implemented - shows products listed with images.

--- app/src/main/java/com/whoadityanawandar/ecommerce/ui/home/HomeFragment.kt
+++ app/src/main/java/com/whoadityanawandar/ecommerce/ui/home/HomeFragment.kt
@@ -1,20 +1,32 @@
 package com.whoadityanawandar.ecommerce.ui.home
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.whoadityanawandar.eCommerce.ui.home.HomeViewModel
+import com.bumptech.glide.Glide
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.whoadityanawandar.ecommerce.ui.home.HomeViewModel
 import com.whoadityanawandar.ecommerce.databinding.FragmentHomeBinding
 import com.whoadityanawandar.ecommerce.R
+import com.whoadityanawandar.ecommerce.model.Product
+import com.whoadityanawandar.ecommerce.viewholder.ProductViewHolder
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
+    private lateinit var productsRef: DatabaseReference
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -31,12 +43,60 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val recyclerView = root.findViewById<RecyclerView>(R.id.recvwProducts)
 
-/*        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })*/
+        productsRef = FirebaseDatabase.getInstance().reference.child("Products")
+
+        val recyclerView = root.findViewById<RecyclerView>(R.id.recvwProducts)
+        var layoutManager = LinearLayoutManager(this.context)
+        recyclerView.layoutManager = layoutManager
+
+        val options: FirebaseRecyclerOptions<Product> = FirebaseRecyclerOptions.Builder<Product>()
+            .setQuery(this.productsRef, Product::class.java)
+            .build()
+
+        val adapter: FirebaseRecyclerAdapter<Product, ProductViewHolder> =
+            object : FirebaseRecyclerAdapter<Product, ProductViewHolder>(options) {
+                override fun onBindViewHolder(
+                    holder: ProductViewHolder,
+                    position: Int,
+                    model: Product
+                ) {
+                    holder.tvProductName.text = model.productName
+                    holder.tvProductPrice.text = model.price
+                    holder.tvProductDescription.text = model.description
+                    Glide.with(holder.ivProductImage.context)
+                        .load(model.imageUrl)
+                        .into(holder.ivProductImage);
+                    //holder.ivProductImage.setImageURI(Uri.parse(model.imageUrl))
+                }
+
+                override fun onCreateViewHolder(
+                    parent: ViewGroup,
+                    viewType: Int
+                ): ProductViewHolder {
+                    val view: View = LayoutInflater.from(parent.context)
+                        .inflate(R.layout.product_items_layout, parent, false)
+                    return ProductViewHolder(view)
+                }
+            }
+
+/*        val navigationView = root.findViewById<View>(R.id.nav_view) as NavigationView
+        navigationView.setNavigationItemSelectedListener(this)*/
+/*        var sharedPreferences = getSharedPreferences("name", AppCompatActivity.MODE_PRIVATE);
+        var name = sharedPreferences.getString("name", "")
+        var profilePicUrl = sharedPreferences.getString("profilePicUrl", "")
+        var headerView = navigationView.getHeaderView(0)
+        txtvwUsername = headerView.findViewById(R.id.txtvwUsername)
+        txtvwUsername.text = name
+        imgvwProfilePic = headerView.findViewById(R.id.imgvwProfilePic)*/
+        //this.productsRef = FirebaseDatabase.getInstance().reference.child("Products")
+        //recyclerView = root.findViewById<RecyclerView>(R.id.recvwProducts)
+
+        recyclerView.setHasFixedSize(true)
+
+        recyclerView.adapter = adapter
+        adapter.startListening()
+
         return root
     }
 
--- app/src/main/java/com/whoadityanawandar/ecommerce/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/whoadityanawandar/ecommerce/ui/home/HomeViewModel.kt
@@ -1,4 +1,4 @@
-package com.whoadityanawandar.eCommerce.ui.home
+package com.whoadityanawandar.ecommerce.ui.home
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
